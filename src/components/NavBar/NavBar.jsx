d3d6a485e6fb492bfaedf59c11b116d6d74b00a0
Jonathan Lopez <jon.14lam@gmail.com>
2022-06-22 22:14:38 -0500

    Barra nav con bootstrap

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,19 +5,33 @@ import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <div className='barraNav'>
-            <img className='imgLogo' src={Logo} alt="logoTienda" />
 
-            <ul className='listaEnlaces'>
-                <li className='nav-item'> Home </li>
-                <li className='nav-item'> Consolas </li>
-                <li className='nav-item'> VideoJuegos </li>
-                <li className='nav-item'> Accesorios </li>
-                <li className='nav-item'> Monitores </li>
-            </ul>
 
-            <CardWidget />
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <img className='imgLogo' src={Logo} alt="logoTienda" />
+                <a className="navbar-brand" href=".">Home</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href=".">Consolas</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href=".">Videojuegos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href=".">Accesorios</a>
+                        </li>
+                    </ul>
+                </div>
+                <CardWidget />
+            </div>
+        </nav>
+
+
     )
 }
 
